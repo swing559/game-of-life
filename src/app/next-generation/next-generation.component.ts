297ef72e81356f8a0e15ba8a1a2bcfeb1a2ff18d
Andrew <swing2@marshall.edu>
2018-08-19 14:12:11 -0400

    Changing for loop variable definition

--- src/app/next-generation/next-generation.component.ts
+++ src/app/next-generation/next-generation.component.ts
@@ -16,14 +16,14 @@ export class NextGenerationComponent implements OnInit {
 	  /*checks each cell to see if it is alive
 		If it is it adds increments the  alive array 
 		for each of its neighbors*/
-	   for (let lifeRow in this.lives){
-		   for(let lifeColumn in this.lives[lifeRow]){
+	   for (var lifeRow =0; lifeRow<this.lives.length; lifeRow++){
+		   for(var lifeColumn=0; lifeColumn<this.lives[0].length; lifeColumn++){
 			   //checks if the cell is true
 			   if(this.lives[lifeRow][lifeColumn]){
 				   /*Increments the neighbors around the alive cell
 				     The Math.max() and Math.min() functions keep it from 
 					 going out of bounds of the array*/
-				   for(var i = Math.max(0, lifeRow-1); i<=Math.min(+lifeRow+1, this.lives.length-1); i++){
+				   for(var i = Math.max(0, lifeRow-1); i<=Math.min(lifeRow+1, this.lives.length-1); i++){
 						for(var j = Math.max(0, lifeColumn-1); j<=Math.min(+lifeColumn+1, this.lives[lifeRow].length-1); j++){
 							if( i != +lifeRow || j != +lifeColumn){
 								alive[i][j]++;
